7480f2960bd750b80cb88df2bddcb6579d1562eb
bohdan.vasylevych <bohdan.vasylevych@tiebreak.solutions>
2021-05-10 10:56:26 +0300

    select

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,6 +97,13 @@ const Home: React.FC = () => {
       <Typography variant="h5" align="center">
         Перегляд графіку та запис до лікаря
       </Typography>
+      <select>
+        <option value="asas">asaas</option>
+        <option value="asas">asaas</option>
+        <option value="asas">asaas</option>
+        <option value="asas">asaas</option>
+        <option value="asas">asaas</option>
+      </select>
       <Divider light variant="middle" />
       <SearchForm setGroupName={setGroupName} />
       {status === "loading" ? (
